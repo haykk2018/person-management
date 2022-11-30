1dc5b7fbbe214b6ce7c3c8438cea3ec90ad0eef1
hayk <haykk2018@gmail.com>
2022-11-30 12:46:30 +0400

    refresh README.md and controller

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -14,7 +14,7 @@ class PersonController extends Controller
      */
     public function index()
     {
-        //
+        return view('index');
     }
 
     /**
@@ -24,7 +24,7 @@ class PersonController extends Controller
      */
     public function create()
     {
-        //
+        return view('new');
     }
 
     /**
